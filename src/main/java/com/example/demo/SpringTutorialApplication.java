096c883c7ddc9ad657d16350090e4993e8dd56ba
seungguini <sl6369@nyu.edu>
2022-04-04 09:46:10 -0400

    Serve sample GET route with Student class

--- src/main/java/com/example/demo/SpringTutorialApplication.java
+++ src/main/java/com/example/demo/SpringTutorialApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Month;
+import java.time.LocalDate;
+import java.util.*;
 
 @SpringBootApplication
 @RestController // Makes this class serve rest Routes
@@ -15,8 +20,10 @@ public class SpringTutorialApplication {
 
 	// Create a GET route for \hello
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<Student> hello() {
+		return List.of(
+				new Student(1L, "Seunggun", "sl6369@nyu.edu", LocalDate.of(1999, Month.JANUARY, 5), 21)
+		);
 	}
 
 }
